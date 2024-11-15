82c999cd65fd8fac4f31e9061ab12bd56d05572b
강창진 <132541840+KangCJ44@users.noreply.github.com>
2024-11-15 14:58:54 +0900

    [level 1] Title: 추억 점수, Time: 3.22 ms, Memory: 88.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+
+class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        int[] answer = {};
+        HashMap<String, Integer> yearningMap = new HashMap<>();
+        for(int i = 0; i < name.length; i++){
+            yearningMap.put(name[i], yearning[i]);
+        }
+        answer = new int[photo.length];
+        
+        for(int i = 0; i < photo.length; i++){
+            int score = 0;
+            for(String person : photo[i]){
+                score += yearningMap.getOrDefault(person, 0);
+            }
+            answer[i] = score;
+        }
+        return answer;
+    }
+}
